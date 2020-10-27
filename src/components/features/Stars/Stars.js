676101405b4ba7cd752932186ea969b748ea4323
Dawid Perzynski <loginosobisty@o2.pl>
2020-10-28 00:33:54 +0100

    stars component

---
+++ src/components/features/Stars/Stars.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Stars.module.scss';
+
+function Stars() {
+  return (
+    <div className={styles.back}>
+      <div className={styles.night}>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+        <div className={styles.star}></div>
+      </div>
+    </div>
+  );
+}
+
+export default Stars;
