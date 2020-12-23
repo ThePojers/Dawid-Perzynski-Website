c23203bcbff9ff9674056d5d4db43d90c49f133e
Dawid Perzynski <loginosobisty@o2.pl>
2020-12-23 01:40:35 +0100

    Works box template

--- src/components/views/Works/Works.js
+++ src/components/views/Works/Works.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import styles from './Works.module.scss';
 
+
 function Works() {
   return (
     <div className={styles.container}>
       <div className={styles.box}>
-        <div className={styles.boxInside}>
-          <p>dupa</p>
+        <div className={styles.boxImage}>
+          <a href='https://mammamia-pizzeria.herokuapp.com' target='_blanket'><div className={styles.boxOverlay}>
+            <p className={styles.boxOverlayText}>DEMO</p>
+          </div>
+          </a>
+        </div>
+        <div className={styles.boxText}>
+          <p className={styles.title}>About:</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Nibh venenatis cras sed felis eget velit aliquet. Risus in hendrerit gravida rutrum quisque non tellus. Morbi blandit cursus risus at ultrices.
+            Ante in nibh mauris cursus. Volutpat commodo sed egestas egestas. Vestibulum sed arcu non odio</p>
+          <p className={styles.title}>Tech stack:</p>
+          <p>Nibh venenatis cras sed felis eget velit aliquet. Risus in hendrerit gravida rutrum quisque non tellus. Morbi blandit cursus risus at ul</p>
         </div>
       </div>
 
