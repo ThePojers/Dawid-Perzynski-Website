f17739e6af82ba9e81a7d0025dfab158b847ab6c
Dawid Perzynski <loginosobisty@o2.pl>
2020-12-23 12:52:30 +0100

    Add another box right side oriented

--- src/components/views/Works/Works.js
+++ src/components/views/Works/Works.js
@@ -6,10 +6,11 @@ function Works() {
   return (
     <div className={styles.container}>
       <div className={`${styles.box} ${styles.first}`}>
-        <div className={styles.boxImage}>
-          <a href='https://mammamia-pizzeria.herokuapp.com' target='_blanket'><div className={styles.boxOverlay}>
-            <p className={styles.boxOverlayText}>DEMO</p>
-          </div>
+        <div className={`${styles.boxImage} ${styles.pizza}`}>
+          <a href='https://mammamia-pizzeria.herokuapp.com' target='_blanket'>
+            <div className={styles.boxOverlay}>
+              <p className={styles.boxOverlayText}>DEMO</p>
+            </div>
           </a>
         </div>
         <div className={styles.boxText}>
@@ -23,10 +24,11 @@ function Works() {
         </div>
       </div>
       <div className={`${styles.box} ${styles.second}`}>
-        <div className={styles.boxImageSecond}>
-          <a href='https://mammamia-pizzeria.herokuapp.com' target='_blanket'><div className={styles.boxOverlay}>
-            <p className={styles.boxOverlayText}>DEMO</p>
-          </div>
+        <div  className={`${styles.boxImageSecond} ${styles.login}`}>
+          <a href='https://login-interface-react2.herokuapp.com' target='_blanket'>
+            <div className={styles.boxOverlay}>
+              <p className={styles.boxOverlayText}>DEMO</p>
+            </div>
           </a>
         </div>
         <div className={styles.boxText}>
@@ -36,7 +38,7 @@ function Works() {
             Ante in nibh mauris cursus. Volutpat commodo sed egestas egestas. Vestibulum sed arcu non odio</p>
           <p className={styles.title}>Tech stack:</p>
           <p>Nibh venenatis cras sed felis eget velit aliquet. Risus in hendrerit gravida rutrum quisque non tellus. Morbi blandit cursus risus at ul</p>
-          <a href="https://github.com/ThePojers/Project-Pizzeria" target='_blanket'><p>Source Code</p></a>
+          <a href="https://github.com/ThePojers/React-task-Codibly" target='_blanket'><p>Source Code</p></a>
         </div>
       </div>
     </div>
